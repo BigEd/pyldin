3098369ddc24e94de7ee1c0d2257294718cef04c
Alexander.Chukov <Alexander.Chukov@1816dcaa-c784-11de-9d4f-f947ee5921c8>
2009-11-02 17:22:16 +0000

    remove

--- tags/3.2/pyldin601/src/pyldin.h
+++
@@ -1,8 +0,0 @@
-/*
- *
- * Pyldin-601 emulator version 3.1 for Linux,MSDOS,Win32
- * Copyright (c) Sasha Chukov & Yura Kuznetsov, 2000-2004
- *
- */
-
-#define MENU_HEIGHT 26
--- tags/3.2/pyldin601/src/sshot.h
+++
@@ -1,6 +0,0 @@
-#ifndef _SSHOT_H_
-#define _SSHOT_H_
-
-int savepng(unsigned short *data, int width, int height);
-
-#endif
