7272a313c0cce46fb05fe5adceb1c4a27221f529
Alexander.Chukov <Alexander.Chukov@1816dcaa-c784-11de-9d4f-f947ee5921c8>
2011-01-14 22:52:29 +0000

    remove unnecessary headers

--- pyldin601/src/core/swiemu.c
+++ pyldin601/src/core/swiemu.c
@@ -1,12 +1,4 @@
 #include <stdio.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <unistd.h>
-#include <limits.h>
-#include <stdlib.h>
-#include <dirent.h>
-#include <string.h>
-#include <time.h>
 
 #include "core/mc6800.h"
 #include "core/floppy.h"
