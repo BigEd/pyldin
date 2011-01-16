44747ccb1edffb4927c1e59aecd70bdb842dfdfa
Alexander.Chukov <Alexander.Chukov@1816dcaa-c784-11de-9d4f-f947ee5921c8>
2011-01-16 08:21:25 +0000

    board speaker support

--- pyldin601/te-lpc2478/include/config.h
+++ pyldin601/te-lpc2478/include/config.h
@@ -20,6 +20,10 @@
 #define BOARD_LED2_PORT 2
 #define BOARD_LED3_PORT 1
 
+#define PYLDIN_SPEAKER_MASK (1 << 18)	// P1.18
+#define PYLDIN_SPEAKER_FIO FIO1PIN
+#define PYLDIN_SPEAKER_PORT 1
+
 #define SDRAM_SIZE     2250000     // Samsung K4S561632HUC75, 256Mbit(4M x 16 bit x 4 Banks). 
 #define SDRAM_BASE     0xA0000000  // 0xA000 0000 - 0xAFFF FFFF Dynamic memory bank 0.
 #define SRAM_BASE      0x40000000  // 0x4000 0000 - 0x4000 FFFF On chip RAM (64 kB).
