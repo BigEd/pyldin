85136325a31104e8af5495ba5b2ee894579786b1
Alexander.Chukov <Alexander.Chukov@1816dcaa-c784-11de-9d4f-f947ee5921c8>
2011-01-16 08:22:00 +0000

    compile core source files as one file

---
+++ trunk/pyldin601/te-lpc2478/core/core.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <string.h>
+#include <time.h>
+
+#define O_INLINE inline
+
+#include "core/mc6800.h"
+#include "core/swiemu.h"
+#include "core/devices.h"
+#include "core/mc6845.h"
+#include "core/i8272.h"
+#include "core/keyboard.h"
+#include "core/printer.h"
+
+#include "core/mc6800.c"
+#include "core/devices.c"
+#include "core/floppy.c"
+#include "core/keyboard.c"
+#include "core/swiemu.c"
+#include "core/i8272.c"
+#include "core/printer.c"
+#include "core/mc6845.c"
+
+void core_50Hz_irq(void)
+{
+    devices_set_tick50();
+    mc6845_curBlink();
+    mc6800_setIrq(1);
+}
