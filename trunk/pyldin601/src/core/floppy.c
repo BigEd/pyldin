7cdaebf2d2e79b8bac9ca5d597238fcf643136aa
Alexander.Chukov <Alexander.Chukov@1816dcaa-c784-11de-9d4f-f947ee5921c8>
2011-01-18 22:09:09 +0000

    split soft int17 and floppy image support

--- trunk/pyldin601/src/core/floppy.c
+++ trunk/pyldin601/src/core/floppy.c
@@ -49,7 +49,7 @@ static int floppyOp(int Op, int Drive, int Track, int Head, int Sector, unsigned
     return 0xc0;
 }
 
-static int readSector(int Disk, int Track, int Sector, int Head, unsigned char *dst)
+int floppy_readSector(int Disk, int Track, int Sector, int Head, unsigned char *dst)
 {
     if (Track > 79 || Sector > 18) 
 	return 0x40;
@@ -69,7 +69,7 @@ static int readSector(int Disk, int Track, int Sector, int Head, unsigned char *
     return 0;
 }
 
-static int writeSector(int Disk, int Track, int Sector, int Head, unsigned char *src)
+int floppy_writeSector(int Disk, int Track, int Sector, int Head, unsigned char *src)
 {
     if (Track > 79 || Sector > 18) 
 	return 0x40;
@@ -91,7 +91,7 @@ static int writeSector(int Disk, int Track, int Sector, int Head, unsigned char 
     return 0;
 }
 
-static int formaTrack(int Disk, int Track, int Head)
+int floppy_formaTrack(int Disk, int Track, int Head)
 {
     if (Track > 79) 
 	return 0x40;
@@ -116,7 +116,7 @@ static int formaTrack(int Disk, int Track, int Head)
     return 0;
 }
 
-static int init765()
+int floppy_init()
 {
     if (!diskImage[FLOPPY_A])
 	floppyOp(0, FLOPPY_A, 0, 0, 0, NULL);
@@ -125,64 +125,4 @@ static int init765()
 	floppyOp(0, FLOPPY_B, 0, 0, 0, NULL);
 
     return 0;
-}
-
-
-void INT17emulator(byte *A, byte *B, word *X, byte *t, word *PC)
-{
-    byte sect_buf[512];
-    word bukva, i2;
-
-    int devs  = mc6800_memr(*X) & 0x01;
-    int track = mc6800_memr(*X + 1);
-    int head  = mc6800_memr(*X + 2) & 0x01;
-    int sect  = mc6800_memr(*X + 3);
-    int offs  = (mc6800_memr(*X + 4) << 8) | mc6800_memr(*X + 5);
-
-    bukva = mc6800_memr(0xed20) << 8; 
-    bukva |= mc6800_memr(0xed21);
-
-//    if (m601a == 0) 
-	bukva += 81; 
-//    else 
-//	bukva += 159;
-
-    switch(*A) {
-	case 0x80:
-	case 0:
-	    *A = init765(); 
-	    break;
-
-	case 1:
-	    mc6800_memw(bukva, 0x52);
-	    *A = readSector(devs, track, sect, head, sect_buf);
-	    for (i2 = 0; i2 < 512; i2++)
-		mc6800_memw(offs + i2, sect_buf[i2]);
-	    mc6800_memw(bukva, 0x20);
-	    break;
-
-	case 2:	
-	    mc6800_memw(bukva, 0x57);
-	    for (i2 = 0; i2 < 512; i2++)
-		sect_buf[i2] = mc6800_memr(offs + i2);
-	    *A = writeSector(devs, track, sect, head, sect_buf);
-	    mc6800_memw(bukva, 0x20);
-	    break;
-
-	case 3:	
-	    mc6800_memw(bukva, 0x53);
-	    *A = readSector(devs, track, sect, head, sect_buf);
-	    mc6800_memw(bukva, 0x20);
-	    break;
-
-	case 4:	
-	    mc6800_memw(bukva, 0x46);
-	    *A = formaTrack(devs, track, head);
-	    mc6800_memw(bukva, 0x20);
-	    break;
-
-	default: 
-	    *A = 0;
-	    break;
-    }
 }
