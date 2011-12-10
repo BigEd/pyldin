d63b6faa55119011f406588962ecd9469ed7bd73
Alexander.Chukov <Alexander.Chukov@1816dcaa-c784-11de-9d4f-f947ee5921c8>
2011-12-10 21:15:23 +0000

    move address calculator out of sync module

--- trunk/pyldin2012/vgaframebuffer.vhdl
+++ trunk/pyldin2012/vgaframebuffer.vhdl
@@ -9,7 +9,9 @@ port(
 	enable					: in std_logic;
 	mode						: in std_logic;
 
-	addr_in					: in std_logic_vector(16 downto 0);
+	row						: in std_logic_vector(9 downto 0);
+	column					: in std_logic_vector(9 downto 0);	
+
 	addr_base				: in std_logic_vector(15 downto 0);
 	addr_out					: out std_logic_vector(15 downto 0);
 	data_in					: in std_logic_vector( 7 downto 0);
@@ -21,24 +23,30 @@ port(
 end vgaframebuffer;
 
 architecture vgaframebuffer_arch of vgaframebuffer is
+signal video_addr			: std_logic_vector(16 downto 0);
 signal data					: std_logic_vector( 7 downto 0);
-signal video_pixel		: std_logic;
+signal pixel				: std_logic;
 begin
-	process(clk, addr_base, addr_in)
+	process(clk, addr_base, video_addr, row, column)
 	begin
-		addr_out <= addr_base + addr_in(16 downto 3);
+--		if (mode = '1') then
+			video_addr <= std_logic_vector("101000000"*row(8 downto 1) + column(9 downto 1));
+			addr_out <= addr_base + video_addr(16 downto 3);
+--		else
+--			addr_out <= addr_base + addr_in();
+--		end if;
 
 		if (clk'event and clk = '0') then
-			if (addr_in(2 downto 0) = "000") then
+			if (video_addr(2 downto 0) = "000") then
 				data <= data_in;
 			else
 				data(7 downto 1) <= data(6 downto 0);
 			end if;
-			video_pixel <= data(7);
+			pixel <= data(7);
 			if (enable = '1') then
-				vga_r(2) <= video_pixel; 
-				vga_g(2) <= video_pixel;
-				vga_b(1) <= video_pixel;
+				vga_r(2) <= pixel; 
+				vga_g(2) <= pixel;
+				vga_b(1) <= pixel;
 			else
 				vga_r <= "000"; 
 				vga_g <= "000";
